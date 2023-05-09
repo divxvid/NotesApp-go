b252fb43e8686743ec0770f5e75c3a4fa5172e9b
Divyansh <divyansh.shrivastava@students.iiit.ac.in>
2023-05-10 00:16:31 +0530

    Add routes and route handlers

---
+++ handlers.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello from the golang server!")
+}
+
+func SignupHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "POST /signup")
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "POST /login")
+}
+
+func LogoutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "GET /logout")
+}
+
+func NotesHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		GetAllNotesHandler(w, r)
+	} else if r.Method == "POST" {
+		CreateNewNoteHandler(w, r)
+	} else {
+		fmt.Fprint(w, "Method not supported!")
+	}
+}
+
+func NotesWithIdHandler(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	if r.Method == "GET" {
+		GetNoteByIdHandler(id, w, r)
+	} else if r.Method == "DELETE" {
+		DeleteNoteByIdHandler(id, w, r)
+	} else {
+		fmt.Fprint(w, "Method not supported")
+	}
+}
+
+func GetAllNotesHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "GET /notes")
+}
+
+func CreateNewNoteHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "POST /notes")
+}
+
+func GetNoteByIdHandler(id string, w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "GET /notes/%s", id)
+}
+
+func DeleteNoteByIdHandler(id string, w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "DELETE /notes/%s", id)
+}
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+const address string = "127.0.0.1:8000"
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", RootHandler)
+	r.HandleFunc("/signup", SignupHandler).Methods("POST")
+	r.HandleFunc("/login", LoginHandler).Methods("POST")
+	r.HandleFunc("/logout", LogoutHandler).Methods("GET")
+	r.HandleFunc("/notes", NotesHandler).Methods("GET", "POST")
+	r.HandleFunc("/notes/{id}", NotesWithIdHandler).Methods("GET", "DELETE")
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         address,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Print("Server starting at ", address)
+	log.Fatal(srv.ListenAndServe())
+}
